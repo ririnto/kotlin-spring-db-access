b31a1be7ff9d3588faf3f0c5cc9a5a19692dc3c5
이학재 <ririnto@kakao.com>
2023-03-11 19:49:58 +0900

    Initial Commit.

---
+++ build.gradle.kts
@@ -0,0 +1,35 @@
+plugins {
+    id("org.springframework.boot") version "3.0.4"
+    id("io.spring.dependency-management") version "1.1.0"
+    kotlin("jvm") version "1.7.22"
+    kotlin("plugin.spring") version "1.7.22"
+    kotlin("plugin.jpa") version "1.7.22"
+}
+
+group = "kotlin.spring.db.access"
+version = "0.0.1-SNAPSHOT"
+java.sourceCompatibility = JavaVersion.VERSION_17
+
+allprojects {
+    repositories {
+        mavenCentral()
+    }
+
+    dependencies {
+        implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+        implementation("org.springframework.boot:spring-boot-starter-data-r2dbc")
+        implementation("org.springframework.boot:spring-boot-starter-webflux")
+        implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
+
+        implementation("io.projectreactor.kotlin:reactor-kotlin-extensions")
+
+        implementation("org.jetbrains.kotlin:kotlin-reflect")
+        implementation("org.jetbrains.kotlinx:kotlinx-coroutines-reactor")
+
+        runtimeOnly("org.postgresql:postgresql")
+        runtimeOnly("org.postgresql:r2dbc-postgresql")
+
+        testImplementation("org.springframework.boot:spring-boot-starter-test")
+        testImplementation("io.projectreactor:reactor-test")
+    }
+}
---
+++ readme.md
@@ -0,0 +1,9 @@
+# kotlin-spring-db-access
+
+- kotlin coroutine 등과 함께 db 접근 시 사용 가능한 dependency 확인
+
+---
+
+- Kotlin 1.7.22
+- JDK 17
+- Spring Boot 3.0.4
